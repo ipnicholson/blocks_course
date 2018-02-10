80b60a5647c1f94baac814f03f6a48c96d00011d
Ian Nicholson <ipnicholson@gmail.com>
2018-02-10 13:28:05 -0800

    refinements for readability

--- block_variables.rb
+++ block_variables.rb
@@ -1,11 +1,13 @@
-# 1. block param is unavailble in surrounding scope 
+# (1). block param is unavailble in surrounding scope
+
 # 1.upto(5) do |n|
 #   puts "Run #{n}"
 # end
 
 # puts n # NameError
 
-# 2. vars instantiated inside block are unavailble in surrounding scope 
+# (2). vars instantiated inside block are unavailble in surrounding scope 
+
 # 1.upto(5) do |n|
 #   wife = "DIANA"
 #   puts "Run #{n}"
@@ -13,14 +15,16 @@
 
 # puts wife # NameError
 
-# 3. vars declared in surrounding scope are available in block scope
+# (3). vars declared in surrounding scope are available in block scope
+
 # name = "Ian"
 
 # 1.upto(5) do |n; name|
 #   puts "Run #{n}, #{name}"
 # end
 
-# 3. vars declared in surrounding scope are available in block scope, and can be overwritten
+# (4). vars declared in surrounding scope are available in block scope, and can be overwritten
+
 # name = "Ian"
 
 # 1.upto(5) do |n|
@@ -30,7 +34,8 @@
 
 # puts name # "LUKE"
 
-# 4. vars can be protected in block scope, they won't overwrite vars in surrounding scope
+# (5). vars can be protected in block scope, they won't overwrite vars in surrounding scope
+
 name = "Ian"
 
 1.upto(5) do |n; name| # name is a protected variable
