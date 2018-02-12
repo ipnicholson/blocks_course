93d96ecd51d6b1e4501003dea72db294af02e7ae
Ian Nicholson <ipnicholson@gmail.com>
2018-02-12 12:29:24 -0800

    move class Promotion to separate file

--- flyer.rb
+++ flyer.rb
@@ -19,28 +19,6 @@ flyers = []
   flyers << Flyer.new("Flyer #{n}", "flyer#{n}@example.com", 45_000 + n * 2300)
 end
 
-class Promotion
-
-  PROMOTIONS = {
-    "United" => 1.5,
-    "Lufthansa" => 2.0,
-    "Delta" => 2.5
-  }
-
-  def initialize(flyers) # array of Flyers objects
-    @flyers = flyers
-  end
-
-  def print
-    @flyers.each do |flyer|
-      PROMOTIONS.each do |airline, multiplier|
-        puts "#{flyer.name} could earn #{flyer.miles_flown * multiplier} miles by flying #{airline}!"
-      end
-    end
-  end
-
-end
-
 my_promotions = Promotion.new(flyers)
 
 my_promotions.print
