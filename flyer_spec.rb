f4ca96c867d91d473dc533a77a3e069122952e4b
Ian Nicholson <ipnicholson@gmail.com>
2018-02-13 09:08:19 -0800

    add tests for new attributes

--- flyer_spec.rb
+++ flyer_spec.rb
@@ -17,11 +17,28 @@ class FlyerTest < Minitest::Test
     expected = "jq@example.com"
     assert_equal expected, result
   end
+
   def test_a_flyer_has_a_miles_flown_attribute
     flyer = Flyer.new("John Q. Public", "jq@example.com", 1_234)
 
     result = flyer.miles_flown
     expected = 1_234
+    assert_equal expected, result
+  end
+
+  def test_a_flyer_has_a_status_attribute
+    flyer = Flyer.new("John Q. Public", "jq@example.com", 1_234, :silver)
+
+    result = flyer.status
+    expected = :silver
+    assert_equal expected, result
+  end
+
+  def test_a_flyer_has_a_default_status_of_bronze
+    flyer = Flyer.new("John Q. Public", "jq@example.com", 1_234)
+
+    result = flyer.status
+    expected = :bronze
     assert_equal expected, result
   end
 end
